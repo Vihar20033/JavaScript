eb408e4e1387c3ad5e516103ded321cacaad23f2
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-01 12:23:50 +0000

    new

---
+++ Basics/operation.js
@@ -0,0 +1,19 @@
+let value = 3
+let negative = -value 
+console.log(negative);
+
+let x = 16 + 1 + "Volvo";
+console.log(x);
+let y = "Volvo" + 16 + 1;
+console.log(y);
+
+console.log(true);
+console.log(+true);     // 1
+console.log(+"");       // 0
+
+// Learn post and prefix from mdn and implement here 
+
+
+
+
+
