bfa14d3741f83be7b950b4c8a39d7fd06956ea4d
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-01 15:23:06 +0000

    Comparision in JavaScript

---
+++ Basics/06_comparision.js
@@ -0,0 +1,21 @@
+console.log("For Null :");
+
+
+console.log( null == 0);
+console.log( null < 0);
+console.log( null >= 0);
+
+console.log("For Undefined :");
+
+
+console.log( undefined == 0);
+console.log( undefined < 0)
+console.log( undefined >= 0)
+
+
+console.log("For general comparision :");
+
+console.log("2" == 2)       // Check only values
+console.log("2" === 2)      // Checks data type also
+
+
