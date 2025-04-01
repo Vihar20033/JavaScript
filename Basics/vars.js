614a7cc813726795e8a786770fd02da6dc6427b6
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-01 10:48:30 +0000

    Add Comments

--- Basics/vars.js
+++ Basics/vars.js
@@ -11,6 +11,16 @@ feild = "Cp"
 /*
 Prefer not to use var
 because it cause issue i function scope and block scope
+
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers
 */
 
 console.table([city,field,name,ID])
