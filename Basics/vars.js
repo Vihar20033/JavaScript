45bfa8122ee7bc47f5f25e89a226d998e9435eee
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-01 08:50:40 +0000

    Basics

---
+++ Basics/vars.js
@@ -0,0 +1,16 @@
+const ID = 12202040501062 //Not Changeable 
+var city = "Anand"
+let field = "Computer"
+name = "Vihar"
+
+city = "Vidhyanagr"
+feild = "Cp"
+
+// id = 2 not allowed
+
+/*
+Prefer not to use var
+because it cause issue i function scope and block scope
+*/
+
+console.table([city,field,name,ID])
