83bca954252fee9bf9556cb55ac860518a762038
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-06 19:03:07 +0000

    Functions

---
+++ Functions/01.js
@@ -0,0 +1,16 @@
+function addtwoNumbers (num1 , num2)    // Parameters
+{
+    console.log(num1 + num2)
+}
+
+const result = addtwoNumbers(2,3)   // Arguments
+console.log(result);                // Result undefined
+
+
+function twoNumbers (num1 , num2)    // Parameters
+{
+    return (num1 + num2)
+}
+
+const results = twoNumbers(2,3)   // Arguments
+console.log(results);                // Stored
