ea350123e6704021e8775b6ccf47b2762d106400
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-06 18:40:56 +0000

    json

---
+++ 02_Basics/05_json.js
@@ -0,0 +1,21 @@
+const c = {
+    course : "Javascript",
+    price : "free"
+}
+
+//c.course
+
+// Multiple time acess
+
+const {course: cs} = c
+console.log(cs)
+
+// values came from backend store in json 
+
+/*
+{
+    "name" : "Vihar"
+    "Number" : "9"
+}
+
+*/
