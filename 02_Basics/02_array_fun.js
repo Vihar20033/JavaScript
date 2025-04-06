be386046b683cfb96d233b81d95503c1e72eeb43
Chudasama Vihar <chudasamavihar547@gmail.com>
2025-04-06 05:01:13 +0000

    Array Function

---
+++ 02_Basics/02_array_fun.js
@@ -0,0 +1,13 @@
+const m = [1,2,3,4]
+const n = [5,6,7]
+
+// m.push(n)
+// const x = m.concat(n)
+// console.log(x);
+
+const y = [...m,...n]
+// console.log(y)
+
+console.log(Array.isArray(['A,D']))
+
+console.log(Array.from('Vihar'))
